9af6d8bea712a8552bfe5b594226e45fa23e383e
Sora Liu <soraliu.dev@gmail.com>
2026-04-29 21:29:43 +0800

    feat: add tailscale

--- home/modules/sys/network/default.nix
+++ home/modules/sys/network/default.nix
@@ -1,11 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, unstablePkgs, ... }:
 
 {
-  home.packages = with pkgs; [
+  home.packages = (with pkgs; [
     iperf3
     hey
     dog # modern DNS client, replaces dogdns, Github: https://github.com/ogham/dog
     bandwhich # show network usage by process, need be executed by `sudo bandwhich`, Github: https://github.com/imsnif/bandwhich
     htop-vim # better top, Github: https://github.com/KoffeinFlummi/htop-vim
-  ];
+  ]) ++ (with unstablePkgs; [
+    tailscale
+  ]);
 }
